ec6b9c768094a98301ab5ca236613890907153e4
Matthieu Coudron <mcoudron@hotmail.com>
2021-07-28 00:53:06 +0200

    feat: added flake support
    and use flake compat to keep compatibility with nix-shell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Luarocks flake";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+
+
+    packages.luarocks = pkgs.luarocks;
+
+    defaultPackage = self.packages."${system}".luarocks;
+    # devShell = pkgs.mkShell {
+    #   name = "luarocks-dev";
+    #   buildInputs = [
+    #     self.packages."${system}".luarocks.inputAttrs
+    #     pkgs.sumneko-lua-language-server
+    #   ];
+    # };
+  });
+}
